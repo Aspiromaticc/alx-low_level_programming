c0efc08686077a1a9e076a4a9f303567277e14f7
Aspiromaticc <aderemiadetunji01@gmail.com>
2022-10-13 16:49:53 -0700

    C prog

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   * 
- *    * Description: 'the program's description'
- *     * 
- *      * Return: Always 0 (Success)
- *       */
+ *  *main- Serves as the entry point for the program
+ *   *
+ *    *Return: By default returns zero if no erros otherwise returns non-zero value.
+ *     */
 
 int main(void)
 {

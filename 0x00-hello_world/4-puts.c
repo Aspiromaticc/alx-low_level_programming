7ee914ec071a590486b437d06b18d3fcfcaff440
Aspiromaticc <aderemiadetunji01@gmail.com>
2022-10-13 17:03:22 -0700

    C prog

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+*Serves as the entry point for the program
+*Return: By default returns zero if error does not occur
+**/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
